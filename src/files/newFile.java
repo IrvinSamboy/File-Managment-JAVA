865e7251f78a6902b0f1a55495490d7494690f54
Irvin Samboy <contacoirvinsamboy@gmail.com>
2024-12-29 23:01:57 -0400

    Creating new file class

---
+++ src/files/newFile.java
@@ -0,0 +1,26 @@
+package files;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class newFile {
+    public static void main(String[] args) {
+        String fileName = "newFile.txt";
+        File file = new File(fileName);
+
+        try{
+            if(file.exists()){
+                System.out.println("File already exists");
+            }
+            else {
+                PrintWriter arro = new PrintWriter(new FileWriter(file));
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
